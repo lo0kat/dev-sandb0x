53660911ef0b86c51e6707744ee66766dfd2ad10
lo0kat <louis.chomel@gmail.com>
2022-12-01 23:21:39 +0100

    init tf + add libvirt provider

---
+++ vm-prov/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  ## Configuration options
+  uri = "qemu:///system"
+  #alias = "server2"
+  #uri   = "qemu+ssh://root@192.168.100.10/system"
+}
